2fd4362a14c6695d45c1aa4aafd950fa34f845a2
Rogel SJ Corral <51706135+rogelcorral@users.noreply.github.com>
2020-07-09 16:08:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,82 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+    music.baDing.play()
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -200
+        music.jumpUp.play()
+    }
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+tiles.setTilemap(tiles.createTilemap(
+            hex`0a0008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000101010101010101010101010101010101010101`,
+            img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`,
+            [myTiles.tile0,sprites.builtin.brick,sprites.builtin.oceanDepths0,sprites.builtin.oceanDepths1,sprites.builtin.oceanDepths6],
+            TileScale.Sixteen
+        ))
+mySprite = sprites.create(img`
+. . . . . . . f f f f f . . . . 
+. . . . . . f e e e e e f . . . 
+. . . . . f e e e d d 3 d f . . 
+. . . . f f e e d d f d d f . . 
+. . . f d d e e d d f d d d c . 
+. . . c d b e e d d d d e e d c 
+. . . c d b e e d d c d d d d c 
+. . . f c f e e d d d f f f f c 
+. . . . f e e e e f f f b 1 f . 
+. . . . f e e f f f e f 1 1 f . 
+. f f . f f f e e e e f f f . . 
+. f e . f f e e e e f e e f . . 
+. f e f f f f f f f e e e f f . 
+. f e f f f b b f e e f 1 b f . 
+. f f f f b d d e e f f 1 1 f . 
+. . f f f f f f f f f f f f f . 
+`, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 5))
+mySprite.ay = 500
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSide(img`
+d e e e e e e d 
+e e e e e e e e 
+d d d d d d d d 
+e e e e e e e e 
+e e e e e e e e 
+d d d d d d d d 
+e e e e e e e e 
+d e e e e e e d 
+`, Math.randomRange(-100, -80), 0)
+    tiles.placeOnTile(projectile, tiles.getTileLocation(9, 5))
+    info.changeScoreBy(1)
+})
